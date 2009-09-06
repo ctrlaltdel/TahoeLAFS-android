899b8cabf4378e0083b08646c56975115b0be91e
Francois Deppierraz <francois@ctrlaltdel.ch>
2009-09-06 14:39:21 +0200

    Include code taken from org.openintents.filemanager

--- gen/org/allmydata/tahoelafs/R.java
+++ gen/org/allmydata/tahoelafs/R.java
@@ -9,34 +9,152 @@ package org.allmydata.tahoelafs;
 
 public final class R {
     public static final class array {
-        public static final int about_authors=0x7f060000;
+        public static final int about_artists=0x7f070003;
+        public static final int about_authors=0x7f070000;
+        /**  Add your name here if you helped out 
+ <item></item> 
+         */
+        public static final int about_documenters=0x7f070001;
+        /**  Add your name here if you helped out, but only in the arrays.xml for the localization that you helped to translate into! 
+ <item></item>  
+         */
+        public static final int about_translators=0x7f070002;
     }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_launcher_folder=0x7f020000;
+        public static final int ic_launcher_folder_open=0x7f020001;
+        public static final int ic_launcher_home=0x7f020002;
+        public static final int ic_launcher_home_small=0x7f020003;
+        public static final int ic_menu_back_small=0x7f020004;
+        public static final int ic_menu_forward_small=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int icon_file=0x7f020007;
+        public static final int icon_sdcard=0x7f020008;
+        public static final int icon_sdcard_small=0x7f020009;
     }
     public static final class id {
-        public static final int Button01=0x7f070001;
-        public static final int FrameLayout01=0x7f070000;
+        public static final int Button01=0x7f080001;
+        public static final int FrameLayout01=0x7f080000;
+        public static final int button1=0x7f080007;
+        public static final int button2=0x7f080008;
+        public static final int button_directory_pick=0x7f08000c;
+        public static final int button_pick=0x7f08000e;
+        public static final int container2=0x7f080006;
+        public static final int directory_buttons=0x7f080009;
+        public static final int directory_input=0x7f08000a;
+        public static final int directory_text=0x7f08000b;
+        public static final int empty=0x7f08000f;
+        public static final int empty_text=0x7f080010;
+        public static final int filename=0x7f08000d;
+        public static final int foldername=0x7f080003;
+        public static final int foldernametext=0x7f080002;
+        public static final int icon=0x7f080012;
+        public static final int info=0x7f080013;
+        public static final int scan_progress=0x7f080011;
+        public static final int text=0x7f080005;
+        public static final int text1=0x7f080004;
     }
     public static final class layout {
         public static final int browser=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int settings=0x7f030002;
+        public static final int dialog_new_folder=0x7f030001;
+        public static final int eula=0x7f030002;
+        public static final int filelist=0x7f030003;
+        public static final int filelist_item=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int settings=0x7f030006;
     }
     public static final class raw {
-        public static final int license_short=0x7f040000;
+        public static final int license_short=0x7f050000;
     }
     public static final class string {
-        public static final int about_comments=0x7f050003;
-        public static final int about_copyright=0x7f050004;
-        public static final int about_email=0x7f050007;
-        public static final int about_website_label=0x7f050005;
-        public static final int about_website_url=0x7f050002;
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
-        public static final int license_short=0x7f050006;
-        public static final int settings=0x7f050008;
+        /**  About 
+         */
+        public static final int about=0x7f06002f;
+        /**  ***************************
+	     Preferences
+	     *************************** 
+         */
+        public static final int about_comments=0x7f060038;
+        /**  ***************************
+	     About information
+	     *************************** 
+         */
+        public static final int about_copyright=0x7f060032;
+        public static final int about_email=0x7f060034;
+        public static final int about_website_label=0x7f060039;
+        public static final int about_website_url=0x7f060033;
+        public static final int aboutapp_get=0x7f060031;
+        /**  About 
+         */
+        public static final int aboutapp_market_uri=0x7f060037;
+        public static final int aboutapp_not_available=0x7f060030;
+        public static final int app_name=0x7f060001;
+        public static final int application_not_available=0x7f060026;
+        public static final int copied_file_name=0x7f06001a;
+        public static final int copied_file_name_2=0x7f06001b;
+        public static final int copy_button=0x7f060019;
+        public static final int copy_title=0x7f060018;
+        public static final int create_new_folder=0x7f06000a;
+        public static final int error_copying_file=0x7f060023;
+        public static final int error_creating_new_folder=0x7f06001c;
+        public static final int error_deleting_file=0x7f06001d;
+        public static final int error_file_does_not_exists=0x7f060024;
+        public static final int error_folder_not_empty=0x7f06001e;
+        public static final int error_moving_file=0x7f060021;
+        public static final int error_moving_folder=0x7f060022;
+        public static final int error_renaming_file=0x7f06001f;
+        public static final int error_renaming_folder=0x7f060020;
+        public static final int eula_accept=0x7f060028;
+        public static final int eula_refuse=0x7f060029;
+        /**  ***************************
+       Distribution
+       *************************** 
+ EULA 
+         */
+        public static final int eula_title=0x7f060027;
+        public static final int file_copied=0x7f060015;
+        public static final int file_deleted=0x7f06000f;
+        public static final int file_moved=0x7f060013;
+        public static final int file_name=0x7f06000c;
+        public static final int file_renamed=0x7f060011;
+        public static final int folder_deleted=0x7f060010;
+        public static final int folder_moved=0x7f060014;
+        public static final int folder_name=0x7f06000b;
+        public static final int folder_renamed=0x7f060012;
+        public static final int hello=0x7f060000;
+        public static final int menu_copy=0x7f060009;
+        public static final int menu_delete=0x7f060004;
+        public static final int menu_move=0x7f060008;
+        /**  Taken from OI FileManager 
+         */
+        public static final int menu_new_folder=0x7f060003;
+        public static final int menu_open=0x7f060007;
+        public static final int menu_rename=0x7f060005;
+        public static final int menu_send=0x7f060006;
+        public static final int move_button=0x7f060017;
+        public static final int move_title=0x7f060016;
+        public static final int really_delete=0x7f06000e;
+        public static final int send_not_available=0x7f060025;
+        public static final int settings=0x7f060002;
+        public static final int this_folder_is_empty=0x7f06000d;
+        public static final int update=0x7f06002e;
+        /**  ***************************
+	     Distribution
+	     *************************** 
+ Update 
+         */
+        public static final int update_app_url=0x7f060035;
+        /**  Update 
+         */
+        public static final int update_box_text=0x7f06002a;
+        public static final int update_check_now=0x7f06002b;
+        public static final int update_checker_url=0x7f060036;
+        public static final int update_error=0x7f06002d;
+        public static final int update_get_updater=0x7f06002c;
+    }
+    public static final class xml {
+        public static final int mimetypes=0x7f040000;
     }
 }

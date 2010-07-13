65c1ff7a5ed56656655a84b81eb29bad399038bf
Francois Deppierraz <francois@ctrlaltdel.ch>
2010-07-13 19:43:34 +0200

    Update to latest SDK

--- gen/org/allmydata/tahoelafs/R.java
+++ gen/org/allmydata/tahoelafs/R.java
@@ -33,6 +33,7 @@ public final class R {
         public static final int icon_file=0x7f020007;
         public static final int icon_sdcard=0x7f020008;
         public static final int icon_sdcard_small=0x7f020009;
+        public static final int logo=0x7f02000a;
     }
     public static final class id {
         public static final int Button01=0x7f080001;

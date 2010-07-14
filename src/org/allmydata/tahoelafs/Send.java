b899492d0d15145567ade789c6e3c5fe86480b64
Aubort Jean-Baptiste <rorist@gmail.com>
2010-07-14 15:08:09 +0200

    Send file to the Grid (don't work with ContentProviders (ie.: MediaGallery) but with Uri representing real filesystem path..

--- src/org/allmydata/tahoelafs/Send.java
+++ src/org/allmydata/tahoelafs/Send.java
@@ -1,8 +1,13 @@
 package org.allmydata.tahoelafs;
 
+import android.content.SharedPreferences;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.preference.PreferenceManager;
+import java.io.File;
 
 public class Send extends Activity {
 
@@ -11,8 +16,41 @@ public class Send extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-        Log.v(TAG, "Now send the stuff to the grid");
+        // Exit if wrong action or if no file data
+        Intent intent = getIntent();
+        if(!intent.getAction().equals(Intent.ACTION_SEND) && !intent.hasExtra(Intent.EXTRA_STREAM)){
+            Log.i(TAG, "No valid intent data");
+            finish();
+        }
+
+        // Get intent data and upload file
+        Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if(uri != null){
+            Log.i(TAG, "uri="+uri.getPath());
+            File file = new File(uri.getPath());
+            if (file.canRead()) {
+                // Get Node and Capability
+                String node = prefs.getString(Prefs.KEY_NODE, getString(R.string.test_node));
+                String cap = prefs.getString(Prefs.KEY_ROOTCAP, getString(R.string.test_rootcap));
+                // UPload file
+                TahoeClient tahoe = new TahoeClient(node);
+                try {
+                    Log.i(TAG, "filename="+file.getName());
+                    Log.i(TAG, "filepath="+file.getCanonicalPath());
+                    tahoe.uploadFile(cap, file.getName(), file.getCanonicalPath());
+                } catch (Exception e) {
+                    Log.e(TAG, e.getMessage());
+                }
+            } else {
+                Log.i(TAG, "file is not readable");
+            }
+        } else {
+            Log.i(TAG, "uri is null");
+        }
+
+
     }
 
 }

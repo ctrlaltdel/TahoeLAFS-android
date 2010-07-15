54c022f86f6137636a5f1cc7f02c129fe1775f40
Aubort Jean-Baptiste <aubort.jeanbaptiste@gmail.com>
2010-07-15 18:55:27 +0200

    Send a file from ContentProviders with an InputStream

--- src/org/allmydata/tahoelafs/Send.java
+++ src/org/allmydata/tahoelafs/Send.java
@@ -1,17 +1,22 @@
 package org.allmydata.tahoelafs;
 
-import android.content.SharedPreferences;
+import java.io.File;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.preference.PreferenceManager;
-import java.io.File;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Send extends Activity {
 
     private final static String TAG = "Send";
+    private static final String[] COLUMNS = { OpenableColumns.DISPLAY_NAME, OpenableColumns.SIZE };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,37 +25,76 @@ public class Send extends Activity {
 
         // Exit if wrong action or if no file data
         Intent intent = getIntent();
-        if(!intent.getAction().equals(Intent.ACTION_SEND) && !intent.hasExtra(Intent.EXTRA_STREAM)){
+        if (!intent.getAction().equals(Intent.ACTION_SEND) && !intent.hasExtra(Intent.EXTRA_STREAM)) {
             Log.i(TAG, "No valid intent data");
             finish();
+        }
+        if (intent.getCategories() != null) {
+            Log.i(TAG, "categories=" + intent.getCategories().toString());
         }
 
         // Get intent data and upload file
         Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-        if(uri != null){
-            Log.i(TAG, "uri="+uri.getPath());
-            File file = new File(uri.getPath());
-            if (file.canRead()) {
-                // Get Node and Capability
-                String node = prefs.getString(Prefs.KEY_NODE, getString(R.string.test_node));
-                String cap = prefs.getString(Prefs.KEY_ROOTCAP, getString(R.string.test_rootcap));
-                // UPload file
-                TahoeClient tahoe = new TahoeClient(node);
-                try {
-                    Log.i(TAG, "filename="+file.getName());
-                    Log.i(TAG, "filepath="+file.getCanonicalPath());
-                    tahoe.uploadFile(cap, file.getName(), file.getCanonicalPath());
-                } catch (Exception e) {
-                    Log.e(TAG, e.getMessage());
+        if (uri != null) {
+            Log.i(TAG, "uri=" + uri.getPath());
+
+            // Get Node and Capability
+            String node = prefs.getString(Prefs.KEY_NODE, getString(R.string.test_node));
+            String cap = prefs.getString(Prefs.KEY_ROOTCAP, getString(R.string.test_rootcap));
+
+            String name = null;
+            String scheme = uri.getScheme();
+            TahoeClient tahoe = new TahoeClient(node);
+
+            if (scheme != null) {
+                Log.v(TAG, "scheme=" + scheme);
+                if (scheme.equals("content")) {
+                    // Content: URI
+                    long size = 0;
+                    Cursor c = managedQuery(uri, COLUMNS, null, null, null);
+                    if (c != null && c.moveToFirst()) {
+                        name = c.getString(0);
+                        size = c.getLong(1);
+                    }
+                    c.close();
+                    // Upload file
+                    try {
+                        Log.i(TAG, "filename=" + name);
+                        //
+                        tahoe
+                                .uploadFile(cap, name, getContentResolver().openInputStream(uri),
+                                        size);
+                    } catch (Exception e) {
+                        Log.e(TAG, e.getMessage());
+                    }
+                } else if (scheme.equals("file")) {
+                    // File: URI
+                    name = uri.getLastPathSegment();
+                    String path = uri.getPath();
+                    if (path != null) {
+                        File file = new File(path);
+                        if (file.canRead()) {
+                            // Upload file
+                            try {
+                                Log.i(TAG, "filename=" + name);
+                                Log.i(TAG, "filepath=" + file.getCanonicalPath());
+                                tahoe.uploadFile(cap, name, file.getCanonicalPath());
+                            } catch (Exception e) {
+                                Log.e(TAG, e.getMessage());
+                            }
+                        } else {
+                            Log.e(TAG, "file is not readable");
+                        }
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "File not supported: " + scheme,
+                            Toast.LENGTH_SHORT);
                 }
             } else {
-                Log.i(TAG, "file is not readable");
+                Log.i(TAG, "scheme is null");
             }
         } else {
             Log.i(TAG, "uri is null");
         }
-
-
     }
-
 }
--- src/org/allmydata/tahoelafs/TahoeClient.java
+++ src/org/allmydata/tahoelafs/TahoeClient.java
@@ -1,28 +1,34 @@
 package org.allmydata.tahoelafs;
 
 import java.io.IOException;
+import java.io.InputStream;
+
 import org.json.JSONArray;
 
 public class TahoeClient {
-	private static String TAG = "TahoeClient";
-	private String node;
-	private RESTClient rest;
-	
-	public TahoeClient(String url) {
-		node = url;
-		rest = new RESTClient();
-	}
-	
-	public void uploadFile(String dir, String filename, String src) throws Exception {
-		rest.put(node + "/uri/" + dir + "/" + filename, src);
-	}
-	
-	public void downloadFile(String cap, String dst) throws IOException {
-		rest.download(node + "/uri/" + cap, dst);
-	}
-	
-	public TahoeDirectory getDirectory(String cap) throws Exception {
-		JSONArray json = rest.getJSON(node + "/uri/" + cap + "/?t=json");
-		return new TahoeDirectory(json);	
-	}
+    // private static String TAG = "TahoeClient";
+    private String node;
+    private RESTClient rest;
+
+    public TahoeClient(String url) {
+        node = url;
+        rest = new RESTClient();
+    }
+
+    public void uploadFile(String dir, String filename, String src) throws Exception {
+        rest.put(node + "/uri/" + dir + "/" + filename, src);
+    }
+
+    public void uploadFile(String dir, String filename, InputStream src, long len) throws Exception {
+        rest.put(node + "/uri/" + dir + "/" + filename, src, len);
+    }
+
+    public void downloadFile(String cap, String dst) throws IOException {
+        rest.download(node + "/uri/" + cap, dst);
+    }
+
+    public TahoeDirectory getDirectory(String cap) throws Exception {
+        JSONArray json = rest.getJSON(node + "/uri/" + cap + "/?t=json");
+        return new TahoeDirectory(json);
+    }
 }
